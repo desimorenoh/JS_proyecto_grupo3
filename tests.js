9ee90c9b771e3f2030a21770882dbc20b923f852
Desirée Moreno Hernández <92276733+desimorenoh@users.noreply.github.com>
2024-10-14 10:22:21 +0200

    Update tests.js

--- tests.js
+++ tests.js
@@ -4,12 +4,12 @@ const expect = chai.expect
 describe('Wheel of Doom', () => {
     it('Puedo elegir candidatas aleatoriamente y las quito de la lista', () => {
   
-      let coders = ["Alexia", "Alisa", "Ana C.", "Anna G.", "Candy", "Carmen", "Desirée", "Faby", "Gabrielle", "Gràcia", "Helen", "Joana", "Judith", "Kristina", "Laura C.", "Laura M.", "Marisa", "Rosa", "Sandra", "Sara", "Sonia", "Tamara", "Valentina", "Yuliya"];
+      let coders = ["Luis", "Kevin", "Alex", "Desi", "Marta", "Javi", "Edu"];
       let position = 1;
   
       let coder = seleccionarCoder(coders, position);
   
       expect(coder).to.equal("Alisa")
-      expect(coders).to.have.same.members(["Alexia", "Ana C.", "Anna G.", "Candy", "Carmen", "Desirée", "Faby", "Gabrielle", "Gràcia", "Helen", "Joana", "Judith", "Kristina", "Laura C.", "Laura M.", "Marisa", "Rosa", "Sandra", "Sara", "Sonia", "Tamara", "Valentina", "Yuliya"])
+      expect(coders).to.have.same.members(["Luis", "Alex", "Desi", "Marta", "Javi", "Edu"])
     })
   })
